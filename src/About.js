1a5899fb4172ec4374b930ee2f1454cd37d08a95
Tyler Main <tyler.l.main@gmail.com>
2022-08-30 21:30:02 -0500

    added add and delete button, changed data flow to downward from app instead of isolated to bestbooks

--- src/About.js
+++ src/About.js
@@ -1,10 +1,49 @@
 import { Component } from 'react';
+import Card from 'react-bootstrap/Card';
+import Tylerimg from './img/profile-tyler.jpg'
+import Danimg from './img/profile-dan.png'
 
 class About extends Component {
 
   render() {
-    <h1>STUFFFF!</h1>;
-    return <p>Profile page coming soon</p>;
+		return(
+			<div className='cards-cnt'>
+				<h1>About Super Rad Books!</h1>
+			
+				<Card style={{ width: '18rem' }}>
+
+					<Card.Img variant="top" src={Tylerimg} />
+
+					<Card.Body>
+
+						<Card.Title>Tyler Main</Card.Title>
+
+						<Card.Text>
+							I'm Tyler Main, I enjoy long walks on the beach and relaxing music. Super Rad Books was started by two people who have never met each other in real life but somehow found incredible success among less than tens of people.
+						</Card.Text>
+
+					</Card.Body>
+
+				</Card>
+
+				<Card style={{ width: '18rem' }}>
+
+					<Card.Img variant="top" src={Danimg} />
+
+					<Card.Body>
+
+						<Card.Title>Dan Awesome</Card.Title>
+
+						<Card.Text>
+							I'm Dan and I love skydiving and muskrat hunting. That's as far as I got. 
+						</Card.Text>
+
+					</Card.Body>
+
+				</Card>
+
+			</div>
+		)
   }
 }
 
--- src/BestBooks.js
+++ src/BestBooks.js
@@ -2,52 +2,48 @@ import React from 'react';
 import axios from 'axios';
 import Carousel from 'react-bootstrap/Carousel';
 import bookMockup from './img/book-mockup.jpg';
+import Button from 'react-bootstrap/Button';
+
 
 class BestBooks extends React.Component {
 
-  constructor(props){
-    super(props);
-    this.state = {
-      books: [],
-    };
-  }
-
-  getBooks = async() => {
-    try{
-      let bookData = await axios.get(`${process.env.REACT_APP_SERVER}/books`);
-      this.setState({
-        books: bookData.data
-      });
-    } catch(error){
-      console.log('We have an error: ', error.response);
-    }
-  };
-
-  componentDidMount(){
-    this.getBooks();
-  }
+	handleDelete = async(bookToDelete) => {
+		try {
+			const response = await axios.delete(`${process.env.REACT_APP_SERVER}/books/${bookToDelete._id}`)
+			console.log(response.status);
+			const filteredBooks = this.props.books.filter(book => {
+				return book._id !== bookToDelete._id;
+			})
+			this.props.removeDeletedBook(filteredBooks);
+		} catch(error){
+		console.log(error)
+		}
+	}
 
   render() {
-    let books = this.state.books.map(book => (
-      <Carousel.Item key={book._id}>
+    let books = this.props.books.map(book => (
+      <Carousel.Item className="carousel-item" key={book._id}>
         <img
           className={book._id}
           src={bookMockup}
           alt={book.name}
-          width="600"
         />
         <Carousel.Caption>
           <h3>{book.name}</h3>
           <p>{book.description}</p>
           <p>{book.status}</p>
+				<Button 
+				variant="secondary" 
+				onClick={() => this.handleDelete(book)} >Delete Book</Button>
         </Carousel.Caption>
       </Carousel.Item>
+			
     ));
     return (
       <>
         <h2>My Essential Lifelong Learning &amp; Formation Shelf</h2>
 
-        {this.state.books.length ? (
+        {this.props.books.length ? (
           <Carousel>
             {books}
           </Carousel>
