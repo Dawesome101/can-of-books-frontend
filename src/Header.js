7bbc17c0f2cabe8ab0e57754630ebc40ba8802c0
Dawesome101 <dawesome@danawesome.com>
2022-08-31 13:35:05 -0700

    Made cosmetic adjustments to the layout of the page and added a secondary check to make sure the page rerenders correctly after deleting an entry

--- src/Header.js
+++ src/Header.js
@@ -1,14 +1,17 @@
 import React from 'react';
 import { Navbar, NavItem } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
+import './Header.css'
 
 class Header extends React.Component {
   render() {
     return (
-      <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+      <Navbar className='nav-bar' collapseOnSelect expand="lg" bg="dark" variant="dark">
         <Navbar.Brand>My Favorite Books</Navbar.Brand>
-        <NavItem><Link to="/" className="nav-link">Home</Link></NavItem>
-        <NavItem><Link to="/about" className="nav-link">About</Link></NavItem>
+        <div>
+          <NavItem className='nav-i'><Link to="/" className="nav-link">Home</Link></NavItem>
+          <NavItem className='nav-i'><Link to="/about" className="nav-link">About</Link></NavItem>
+        </div>
       </Navbar>
     );
   }
